d546afa967627182d354d085169d9dc0a132ec85
madhat5 <davidmalary@gmail.com>
2024-11-24 15:54:45 -0500

    feat: setup supabase backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState, useEffect } from 'react'
 
+import { createClient } from "@supabase/supabase-js";
+import { supaKey } from '../config';
+
+  const supabase = createClient("https://vvlkqpqsfrexqrqcoiha.supabase.co", supaKey);
+
+
+function App() {
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(null); 
+
+  useEffect(() => {
+    getPlayers();
+  }, []);
+
+  async function getPlayers() {
+    try {
+      setLoading(true); // Start loading
+      const { data, error } = await supabase.from("nfl_draft").select();
+
+      if (error) throw error; 
+      setPlayers(data);
+
+    } catch (err) {
+      setError(err.message); 
+
+    } finally {
+      setLoading(false); // Stop loading
+    }
+  }
+
+  console.log('players', players);
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {loading && <p>Loading players...</p>}
+        {error && <p>Error: {error}</p>}
+        {!loading && !error && (
+          <ul>
+            {players.map((player) => (
+              <li key={player.key}>{player.playerName}</li>
+            ))}
+          </ul>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
