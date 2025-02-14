04c3bac13cd2ac1a74c4e47db6126b5ea2375fe3
madhat5 <davidmalary@gmail.com>
2025-02-14 09:33:42 -0500

    feat: add state to map

--- components/MapLayer.jsx
+++ components/MapLayer.jsx
@@ -1,23 +1,30 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from 'react';
+
 import Map from 'react-map-gl/mapbox';
-// If using with mapbox-gl v1:
-// import Map from 'react-map-gl/mapbox-legacy';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { glKey } from '../config';
 
 const MapLayer = () => {
+  const [viewState, setViewState] = useState({
+    longitude: -100,
+    latitude: 40,
+    zoom: 3.5
+  });
+
   return (
-    // <div className="h-80 w-80">
     <div >
       <Map
       mapboxAccessToken={glKey}
-      initialViewState={{
-        longitude: -87.6500523,
-        latitude: 41.850033,
-        zoom: 3
-      }}
+      // initialViewState={{
+      //   longitude: -87.6500523,
+      //   latitude: 41.850033,
+      //   zoom: 3.5
+      // }}
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
       style={{width: 800, height: 600}}
       mapStyle="mapbox://styles/mapbox/dark-v11"
       // mapStyle="mapbox://styles/mapbox/streets-v9"
