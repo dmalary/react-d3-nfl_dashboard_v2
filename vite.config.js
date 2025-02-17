f512df5ef8ad37cf63c8c0d338a42771eb70e38f
madhat5 <davidmalary@gmail.com>
2025-02-17 17:57:09 -0500

    build: add gh-pages pkg

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-d3-nfl_dashboard_v2',
   plugins: [react(), tailwindcss()],
 })
